9287b1e67fa02e716995d33cdc03aac4663b6749
zidanmahmoud <mahmud_zz@msn.com>
2021-10-22 00:26:56 +0200

    lint flake8

--- money_tracker/utils.py
+++ money_tracker/utils.py
@@ -34,8 +34,21 @@ class MT:
         self._cu.execute(create_transactions_table)
         self.commit()
 
+    def get_userid_from_username(self, username):
+        usernames = self._get_available_usernames()
+        if username not in usernames:
+            raise ValueError(
+                f"Transaction cannot be added! No username: {username}"
+            )
+        get_id_qr = f"""
+            SELECT user_id, username FROM users WHERE username='{username}'
+        """
+        return pd.read_sql_query(get_id_qr, self._db).iloc[0, 0]
+
     def get_users_df(self):
-        return pd.read_sql_query("SELECT * FROM users", self._db, index_col="user_id")
+        return pd.read_sql_query(
+            "SELECT * FROM users", self._db, index_col="user_id"
+        )
 
     def get_users_df_custom(self, custom_query):
         return pd.read_sql_query(custom_query, self._db, index_col="user_id")
@@ -49,7 +62,9 @@ class MT:
         return pd.read_sql_query(custom_query, self._db, index_col="rowid")
 
     def _get_available_ids(self):
-        return pd.read_sql_query("SELECT user_id FROM users", self._db).values.flatten()
+        return pd.read_sql_query(
+            "SELECT user_id FROM users", self._db
+        ).values.flatten()
 
     def _get_available_usernames(self):
         return pd.read_sql_query(
@@ -60,32 +75,50 @@ class MT:
         ids = self._get_available_ids()
         usernames = self._get_available_usernames()
         if user_id in ids:
-            raise ValueError(
-                f"{user_id} already exists in the database! Please select another user_id"
-            )
+            raise ValueError(f"""
+                {user_id} already exists in the database!
+                Please select another user_id
+            """)
         if username in usernames:
-            raise ValueError(
-                f"{username} already exists in the database! Please select another username"
-            )
-        add_usr_qr = f"INSERT INTO users VALUES({user_id}, '{username}', '{passwd}')"
+            raise ValueError(f"""
+                {username} already exists in the database!
+                Please select another username
+            """)
+        add_usr_qr = f"""
+            INSERT INTO users VALUES({user_id}, '{username}', '{passwd}')
+        """
         self._cu.execute(add_usr_qr)
         self.commit()
 
     def add_transaction_by_userid(self, user_id, amount, date, category):
         ids = self._get_available_ids()
         if user_id not in ids:
-            raise ValueError(f"Transaction cannot be added! No user id: {user_id}")
-        add_trns_qr = f"INSERT INTO transactions VALUES({user_id}, {amount}, '{date}', '{category}')"
+            raise ValueError(
+                f"Transaction cannot be added! No user id: {user_id}"
+            )
+        add_trns_qr = f"""
+            INSERT INTO transactions VALUES(
+                {user_id}, {amount}, '{date}', '{category}'
+            )
+        """
         self._cu.execute(add_trns_qr)
         self.commit()
 
     def add_transaction_by_username(self, username, amount, date, category):
         usernames = self._get_available_usernames()
         if username not in usernames:
-            raise ValueError(f"Transaction cannot be added! No username: {username}")
-        get_id_qr = f"SELECT user_id, username FROM users WHERE username='{username}'"
+            raise ValueError(
+                f"Transaction cannot be added! No username: {username}"
+            )
+        get_id_qr = f"""
+            SELECT user_id, username FROM users WHERE username='{username}'
+        """
         user_id = pd.read_sql_query(get_id_qr, self._db).iloc[0, 0]
-        add_trns_qr = f"INSERT INTO transactions VALUES({user_id}, {amount}, '{date}', '{category}')"
+        add_trns_qr = f"""
+            INSERT INTO transactions VALUES(
+                {user_id}, {amount}, '{date}', '{category}'
+            )
+        """
         self._cu.execute(add_trns_qr)
         self.commit()
 
@@ -95,7 +128,9 @@ class MT:
 
     def remove_user_by_userid(self, user_id):
         if user_id not in self._get_available_ids():
-            warnings.warn(f"user_id {user_id} not found ... did not remove any users")
+            warnings.warn(
+                f"user_id {user_id} not found ... did not remove any users"
+            )
         self._cu.execute(f"DELETE FROM users WHERE user_id={user_id}")
         self._cu.execute(f"DELETE FROM transactions WHERE user_id={user_id}")
         self.commit()

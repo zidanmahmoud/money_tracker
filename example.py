c9027d23dc2da14a9e546d83e5f49f59a3c2ec62
zidanmahmoud <mahmud_zz@msn.com>
2021-10-25 21:06:32 +0200

    change_date_dtype_to_datetime

--- example.py
+++ example.py
@@ -1,13 +1,16 @@
+from datetime import date
 from utils import MT
 
 if __name__ == "__main__":
-    tracker = MT("example_data.db")
-    tracker.initialize_empty_database()
-    tracker.add_user(1, "test", "1234")
-    tracker.add_user(2, "test2", "1234")
-    tracker.add_transaction_by_userid(1, 50, "2020.12.31", "TEST")
-    tracker.add_transaction_by_username("test", 50, "2020.12.31", "TEST")
-    tracker.add_transaction_by_userid(2, 50, "2020.12.31", "TEST")
-    tracker.add_transaction_by_username("test2", 50, "2020.12.31", "TEST")
+    tracker = MT()
+    tracker.open_db("example_data.db")
+    if tracker.is_empty():
+        tracker.initialize_database()
+        tracker.add_user(1, "test", "1234")
+        tracker.add_user(2, "test2", "1234")
+        tracker.add_transaction_by_userid(1, 50, date(2020, 12, 31), "TEST")
+        tracker.add_transaction_by_username("test", 50, date(2020, 12, 31), "TEST")
+        tracker.add_transaction_by_userid(2, 50, date(2020, 12, 31), "TEST")
+        tracker.add_transaction_by_username("test2", 50, date(2020, 12, 31), "TEST")
     print(tracker.get_users_df())
     print(tracker.get_transactions_df())
--- main.py
+++ main.py
@@ -1,34 +1,5 @@
 from utils import MT
+from datetime import date
 
 if __name__ == "__main__":
-
-    instance = MT("DATA.db")
-    instance.initialize_empty_database()
-    # instance.add_user(1, "test", "1234")
-    # instance.add_user(2, "test2", "1234")
-    # instance.add_transaction_by_userid(1, 50, "2020.12.31", "TEST")
-    # instance.add_transaction_by_username("test", 50, "2020.12.31", "TEST")
-    # instance.add_transaction_by_userid(2, 50, "2020.12.31", "TEST")
-    # instance.add_transaction_by_username("test2", 50, "2020.12.31", "TEST")
-
-    print(instance.get_users_df())
-    print("---------------------------\n")
-    print(instance.get_transactions_df())
-
-    only_rent_qr = """
-        SELECT rowid, * FROM transactions
-        WHERE category="TEST"
-    """
-    print("---------------------------\n")
-    print(instance.get_transactions_df_custom(only_rent_qr))
-
-    username = "test"
-    user_id = instance.get_userid_from_username(f"{username}")
-    print(
-        instance.get_transactions_df_custom(f"""
-            SELECT rowid, * FROM transactions
-            WHERE user_id={user_id}
-        """)
-    )
-
-    instance.close()
+    pass
